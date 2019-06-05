6b346d1cea107e3d644c7409d55e09209cab6f06
aleksey197 <azuev3199@gmail.com>
2019-06-06 00:39:15 +0300

    next commit

--- lib/factory.rb
+++ lib/factory.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # * Here you must define your `Factory` class.
 # * Each instance of Factory could be stored into variable. The name of this variable is the name of created Class
 # * Arguments of creatable Factory instance are fields/attributes of created class
@@ -15,4 +17,68 @@
 # - values_at
 # - ==, eql?
 
+# class Factory
+class Factory
+  class << self
+    def new(*parameters, &block)
+      if parameters.first.is_a? String
+        const_set(parameters.shift.capitalize, class_new(*parameters, &block))
+      end
+      class_new(*parameters, &block)
+    end
 
+    def class_new(*parameters, &block)
+      Class.new do
+        attr_accessor(*parameters)
+
+        define_method :initialize do |*arg|
+          parameters.each_index do |index|
+            instance_variable_set("@#{parameters[index]}", arg[index])
+          end
+        end
+
+        def each(&block)
+          to_a.each(&block)
+        end
+
+        define_method :members do
+          parameters
+        end
+
+        def each_pair(&block)
+          members.zip(to_a).each(&block)
+        end
+
+        def ==(other)
+          self.class == other.class && self.to_a == other.to_a
+        end
+
+        alias_method :eql?, :==
+
+        def dig(*keys)
+          keys.inject(self) { |values, key| values[key] if values }
+        end
+
+        def length
+          instance_variables.length
+        end
+
+        alias_method :size, :length
+
+        def select(&block)
+          to_a.select(&block)
+        end
+
+        def to_a
+          instance_variables.map { |item| instance_variable_get item }
+        end
+
+        def values_at(*itm)
+          to_a.select { |val| itm.include?(to_a.index(val)) }
+        end
+
+        class_eval(&block) if block_given?
+      end
+    end
+  end
+end
